b41bc3db9234c7d69c1b7d1b98962a4a6aa4dff8
amalkin <102127060+anmalkin@users.noreply.github.com>
2024-06-29 19:11:48 -0700

    Use gitsigns for lualine diff info.

--- nvim/.config/nvim/after/plugin/gitsigns.lua
+++ nvim/.config/nvim/after/plugin/gitsigns.lua
@@ -31,3 +31,20 @@ require('gitsigns').setup({
         map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
     end
 })
+
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed
+    }
+  end
+end
+
+require'lualine'.setup {
+  sections = {
+    lualine_b = { {'diff', source = diff_source}, },
+  }
+}
